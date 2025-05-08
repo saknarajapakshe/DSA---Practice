170b38604c78aeeaea127b0a667bfe96346fcde1
saknarajapakshe <rajapaksherars.22@uom.lk>
2025-05-08 23:31:08 +0530

    Refactor comments in _LinkedLists.java to improve clarity and organization of advantages, disadvantages, and uses of linked lists

--- _LinkedLists.java
+++ _LinkedLists.java
@@ -56,12 +56,28 @@ public class _LinkedLists {
 }
 
 
-/**********************************************/
+/****************************************************************/
 
-//Advantages of LinkedLists
-    // 1.Dynamic Data Structure - Linked lists can grow and shrink in size dynamically, allowing 
-    //                            for efficient memory usage. (Allocates needed memory while running)
-    //   2.Insertion and deletion of a node is easy.  O(1) time complexity.
-    // 3.Linked lists can be used to implement other data structures, such as stacks, queues, and hash tables.
-    // 4.No/Low memory usage
+/* 
+Advantages of LinkedLists
+    1.Dynamic Data Structure - Linked lists can grow and shrink in size dynamically, allowing 
+                               for efficient memory usage. (Allocates needed memory while running)
+    2.Insertion and deletion of a node is easy.  O(1) time complexity.
+    3.Linked lists can be used to implement other data structures, such as stacks, queues, and hash tables.
+    4.No/Low memory usage
+
+Disadvantages of LinkedLists
+    1.Random access is not possible. O(n) time complexity.
+    2.Linked lists require more memory than arrays due to the overhead of storing pointers.
+    3.Linked lists are less cache-friendly than arrays, which can lead to performance issues in some cases.
+    4.Linked lists are more complex to implement and maintain than arrays.
+    5. Accessing/Searching is slower than arrays. O(n) time complexity.
+
+Uses ??
+    1.Implemet stacks and Queues
+    2.GPS Navigation systems
+    3.Image Viewer
+    4.Music Player
+
+*/
     

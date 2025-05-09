adc73efbaaaefadc0fb9138fef237de8a8916c19
saknarajapakshe <rajapaksherars.22@uom.lk>
2025-05-09 08:58:21 +0530

    advantages and disadvantages of dynamic arrays

---
+++ dynamicArrays.java
@@ -0,0 +1,18 @@
+public class dynamicArrays{
+    public static void main(String[] args) {
+        /*
+         * Advantages of Dynamic Arrays:
+         *    1. Random access of elements. O(1) time complexity.
+            *2. Easy to implement.
+         *  3. Can easily delete and insert at the end 
+         * 4.Good cache locality.
+         * 
+         * 
+         * Disadvantages of Dynamic Arrays:
+         *   1. Waste memory
+         *  2.Shifting elements is more time consuming. O(n) time complexity.
+         * 3. Expanding or shrinking the array is costly. O(n) time complexity.
+         * 4. Insertion and deletion of elements is costly. O(n) time complexity.
+         */
+    }
+}

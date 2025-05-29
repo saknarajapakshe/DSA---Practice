c9237df0026887da9c9bca5a53ebfdc036812341
saknarajapakshe <rajapaksherars.22@uom.lk>
2025-05-29 14:35:11 +0530

    Add traversal methods and LCA functionality to Binary Search Tree; update Main for testing

--- BST/BinarySearchTree.java
+++ BST/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package BST;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
     public Node root;
 
@@ -24,7 +27,7 @@ public class BinarySearchTree {
         }return root;
     }
 
-    public boolean search(int data){
+    public  boolean  search(int data){
         if(searchRec(root,data) == null){
             return false;
         }else {
@@ -46,4 +49,92 @@ public class BinarySearchTree {
             return root;
         }
     }
+
+    //preorder traversal
+    public void preOrder(){
+        preOrderRec(root);
+    }
+
+    private void preOrderRec(Node root){
+        if(root != null){
+            System.out.print(root.data + " ");
+        preOrderRec(root.left);
+        preOrderRec(root.right);
+        }
+    }
+    
+    //inorder traversal
+    public void inOrder(){
+        inOrderRec(root);
+    }
+
+    private void inOrderRec(Node root){
+        if(root!=null){
+            inOrderRec(root.left);
+            System.out.print(root.data + " ");
+            inOrderRec(root.right);
+        }
+    }
+
+    //postOrder Traversal
+    public void postOrder(){
+        postOrderRec(root);
+    }
+
+    private void postOrderRec(Node root){
+        if(root!=null){
+            postOrderRec(root.left);
+            postOrderRec(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public void BFS(Node root){
+        if(root == null){
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            System.out.print(node.data + " ");
+            if(node.left!=null){
+                queue.add(node.left);
+            }
+            if(node.right!=null){
+                queue.add(node.right);
+            }
+        }
+
+       
+    }
+     public void LCA(int data1,int data2){
+            boolean node = search(data1);
+
+            if(node==false){
+                System.out.println("Node " + data1 + " not found in the tree.");
+                return;
+            }
+            boolean node2 = search(data2);
+            if(node2==false){
+                System.out.println("Node " + data2 + " not found in the tree.");
+                return;
+            }
+
+            Node lca = LCARec(root, data1, data2);
+        }
+
+        private Node LCARec(Node root,int data1,int data2){
+            if(data1<root.data && data2<root.data){
+                return LCARec(root.left, data1, data2);
+            }
+            else if(data1>root.data && data2>root.data){
+                return LCARec(root.right, data1, data2);
+            }
+            else return root;
+        }
+
+
 }
--- BST/Main.java
+++ BST/Main.java
@@ -6,7 +6,19 @@ public class Main {
 
         tree.insert(10);
         tree.insert(5);
+        tree.insert(7);
+        tree.insert(42);
+        tree.insert(22);
+        tree.insert(12);
+        tree.insert(16);
+        tree.insert(8);
+
 
         System.out.println("Root: " + tree.root.data);
+        tree.preOrder();
+        System.out.println();
+        tree.inOrder();
+        System.out.println();
+        tree.postOrder();
     }
 }
